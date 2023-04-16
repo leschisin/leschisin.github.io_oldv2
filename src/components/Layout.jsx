8379f861746095320b66734ef58d012735e8e6d3
Steve Leschisin <leschisin@outlook.com>
2023-04-15 21:27:52 -0500

    check in

---
+++ src/components/Layout.jsx
@@ -0,0 +1,15 @@
+import { Outlet, Link } from "react-router-dom";
+import Nav from "./Nav";
+import Footer from "./Footer";
+
+const Layout = () => {
+  return (
+    <>
+      <Nav />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
