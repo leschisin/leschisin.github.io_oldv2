8036b1790b271dd9ffbf2ba1a241ee99b6f6a3cb
Steve Leschisin <leschisin@outlook.com>
2023-04-18 22:07:25 -0500

    check in

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,61 +1,72 @@
-import { useState } from "react";
-import { Dialog } from "@headlessui/react";
+import { useState, Fragment } from "react";
+import { Link } from "react-router-dom";
+import { Popover, Transition, Dialog } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import useScrollPosition from "../hooks/useScrollPosition";
 
-const navigation = [
+const links = [
   { name: "Home", href: "/" },
   { name: "Resume", href: "/resume" },
   { name: "About", href: "/about" },
   { name: "Contact", href: "/contact" },
 ];
 
-import useScrollPosition from "../hooks/useScrollPosition";
-
 export default function Nav() {
   const scrollPosition = useScrollPosition();
+
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
-  const linkStyles = "flex items-center px-6";
-
   return (
-    <header className="fixed top-0 left-0 right-0 z-50">
-      <nav className="mx-auto flex max-w-7xl items-center justify-between">
-        <a href="/" className="-m-1.5 p-1.5">
-          steve.leschisin
-        </a>
-        <div className="flex lg:hidden">
+    <header
+      className={`fixed top-0 left-0 right-0 z-40 flex h-20 px-6 text-white transition-all ${
+        parseInt(scrollPosition) > window.innerHeight - 80
+          ? "bg-black bg-opacity-80 h-14"
+          : ""
+      }`}
+    >
+      <div className="w-full max-w-screen-2xl mx-auto flex justify-between">
+        <div className="flex items-center">
+          <Link
+            to="/"
+            className="hover:border-b-2 border-rose-600 h-7 transition-all"
+          >
+            steve<span className="text-rose-600 text-xl">.</span>leschisin
+          </Link>
+        </div>
+        <nav className="hidden sm:flex items-center">
+          {links.map((link) => (
+            <Link
+              key={link.name}
+              to={link.href}
+              className="ml-12 hover:border-b-2 border-rose-600 h-7 transition-all"
+            >
+              {link.name}
+            </Link>
+          ))}
+        </nav>
+        <nav className="flex sm:hidden items-center">
           <button
             type="button"
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            className="p-2"
             onClick={() => setMobileMenuOpen(true)}
           >
-            <span className="sr-only">Open main menu</span>
+            <span className="sr-only">Open mobile menu</span>
             <Bars3Icon className="h-6 w-6" aria-hidden="true" />
           </button>
-        </div>
-        <div className="hidden lg:flex lg:gap-x-12">
-          {navigation.map((item) => (
-            <a
-              key={item.name}
-              href={item.href}
-              className="text-sm font-semibold leading-6"
-            >
-              {item.name}
-            </a>
-          ))}
-        </div>
-      </nav>
+        </nav>
+      </div>
       <Dialog
         as="div"
-        className="lg:hidden"
+        className="sm:hidden"
         open={mobileMenuOpen}
         onClose={setMobileMenuOpen}
       >
-        <div className="fixed inset-0 z-10" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <div className="fixed inset-0 z-50" />
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
-              steve.leschisin
+            <a href="/" className="-m-1.5 p-1.5">
+              <span className="sr-only">Steve Leschisin</span>
+              <span>steve.leschisin</span>
             </a>
             <button
               type="button"
@@ -69,7 +80,7 @@ export default function Nav() {
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
-                {navigation.map((item) => (
+                {links.map((item) => (
                   <a
                     key={item.name}
                     href={item.href}
@@ -78,6 +89,14 @@ export default function Nav() {
                     {item.name}
                   </a>
                 ))}
+              </div>
+              <div className="py-6">
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Download CV
+                </a>
               </div>
             </div>
           </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,98 @@
 // import styles from "../styles/home.module.css";
-import barnImg from "/barn.JPG"
+import barnImg from "/barn.JPG";
 
 export default function Home() {
   return (
-    <div
-      className="h-screen w-full bg-cover grayscale bg-no-repeat bg-center"
-      style={{ backgroundImage: `url(${barnImg})` }}
-    >
-      this is home
+    <div style={{ height: 9999 }}>
+      <img
+        className="object-cover h-screen w-full saturate-0 brightness-50"
+        src={barnImg}
+        alt="Steve Leschisin"
+      ></img>
+      {/* <div
+        className="h-screen w-full bg-cover bg-no-repeat bg-center saturate-0 brightness-50 bg-fixed"
+        style={{ backgroundImage: `url(${barnImg})` }}
+      >
+        
+      </div> */}
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Scelerisque eleifend donec pretium vulputate. Suspendisse in est ante
+          in nibh. Massa tincidunt nunc pulvinar sapien et ligula ullamcorper
+          malesuada. Vitae proin sagittis nisl rhoncus mattis rhoncus urna
+          neque. Sagittis vitae et leo duis ut diam. Donec et odio pellentesque
+          diam volutpat. Odio aenean sed adipiscing diam donec. Faucibus turpis
+          in eu mi. Viverra suspendisse potenti nullam ac. Placerat vestibulum
+          lectus mauris ultrices eros. Diam vel quam elementum pulvinar etiam
+          non quam. Blandit aliquam etiam erat velit scelerisque in.
+        </p>
+        <p>
+          Tempus urna et pharetra pharetra. Facilisi nullam vehicula ipsum a
+          arcu cursus. Curabitur gravida arcu ac tortor dignissim convallis.
+          Vitae sapien pellentesque habitant morbi. Sem viverra aliquet eget sit
+          amet tellus. Neque egestas congue quisque egestas diam in. Tellus
+          pellentesque eu tincidunt tortor aliquam nulla facilisi cras
+          fermentum. Purus in massa tempor nec feugiat nisl pretium fusce.
+          Pharetra sit amet aliquam id diam maecenas ultricies. Ornare lectus
+          sit amet est placerat in egestas erat. Erat nam at lectus urna duis
+          convallis. Sit amet consectetur adipiscing elit ut aliquam. Massa
+          tincidunt dui ut ornare lectus sit amet est placerat. At augue eget
+          arcu dictum varius duis at consectetur. Nibh nisl condimentum id
+          venenatis a condimentum vitae sapien pellentesque. Dolor sit amet
+          consectetur adipiscing elit pellentesque habitant. Lacus sed viverra
+          tellus in. Mi sit amet mauris commodo quis imperdiet massa tincidunt
+          nunc. Purus ut faucibus pulvinar elementum integer enim neque. Tellus
+          elementum sagittis vitae et.
+        </p>
+        <p>
+          Et sollicitudin ac orci phasellus egestas tellus rutrum tellus. A diam
+          maecenas sed enim ut sem viverra. Arcu ac tortor dignissim convallis
+          aenean et tortor at risus. Luctus accumsan tortor posuere ac ut
+          consequat semper. Dictumst vestibulum rhoncus est pellentesque. Eget
+          mauris pharetra et ultrices. Viverra maecenas accumsan lacus vel
+          facilisis. Duis at consectetur lorem donec massa sapien faucibus.
+          Sagittis vitae et leo duis ut diam quam. Eu facilisis sed odio morbi
+          quis commodo. Varius morbi enim nunc faucibus. Semper risus in
+          hendrerit gravida rutrum quisque non. Vulputate dignissim suspendisse
+          in est ante in nibh mauris cursus. Vel pharetra vel turpis nunc eget.
+          Eget aliquet nibh praesent tristique magna sit amet purus gravida.
+          Nunc mi ipsum faucibus vitae aliquet nec ullamcorper sit amet. Massa
+          vitae tortor condimentum lacinia quis vel eros donec. Eget nunc
+          lobortis mattis aliquam faucibus purus in massa. Faucibus vitae
+          aliquet nec ullamcorper sit amet risus.
+        </p>
+        <p>
+          Nam at lectus urna duis convallis. Proin sed libero enim sed faucibus
+          turpis in eu. Vitae justo eget magna fermentum iaculis eu non diam.
+          Augue mauris augue neque gravida in. Commodo quis imperdiet massa
+          tincidunt nunc pulvinar sapien. Ultrices mi tempus imperdiet nulla
+          malesuada pellentesque elit. Mauris cursus mattis molestie a iaculis.
+          Nulla facilisi morbi tempus iaculis urna id volutpat lacus. Ut
+          venenatis tellus in metus vulputate eu scelerisque felis imperdiet.
+          Sed vulputate mi sit amet mauris commodo quis. Vulputate mi sit amet
+          mauris commodo. Ultrices gravida dictum fusce ut. Natoque penatibus et
+          magnis dis parturient montes nascetur ridiculus mus. Nisl pretium
+          fusce id velit. Vivamus at augue eget arcu dictum varius. At auctor
+          urna nunc id cursus metus. Quam adipiscing vitae proin sagittis nisl
+          rhoncus mattis rhoncus. At quis risus sed vulputate odio ut enim
+          blandit volutpat. Arcu bibendum at varius vel pharetra. Auctor augue
+          mauris augue neque gravida in. Nec sagittis aliquam malesuada bibendum
+          arcu. Dolor magna eget est lorem. Cras semper auctor neque vitae
+          tempus quam. Cursus mattis molestie a iaculis at erat pellentesque.
+          Egestas congue quisque egestas diam in. Ut sem viverra aliquet eget.
+          Odio morbi quis commodo odio aenean sed adipiscing diam donec. At
+          lectus urna duis convallis. Sit amet dictum sit amet justo. Elementum
+          sagittis vitae et leo duis ut.
+        </p>
+      </div>
     </div>
+    // <div
+    //   className="h-screen w-full bg-cover grayscale bg-no-repeat bg-center"
+    //   style={{ backgroundImage: `url(${barnImg})` }}
+    // >
+    //   this is home
+    // </div>
   );
 }
